839111e337af2a3a4a6885b2dbdad898cc6cfd6c
houzhitao <houzhitao@wshifu.com>
2019-05-21 10:45:39 +0800

    siteUrl

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: `houzhitao.netlify.com`,
+    siteUrl: `https://houzhitao.netlify.com/`,
     title: `侯治涛`,
     description: `一个非常漂亮，可爱，善良，但是凶悍的妹纸`,
     author: `monkey`
